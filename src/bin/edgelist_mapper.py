aa074b301c88d052e0d1bd72cdb70ed22dd8605b
simonepri <simonepri@outlook.com>
2020-03-20 08:08:13 +0000

    Create edgelist_mapper.py

---
+++ src/bin/edgelist_mapper.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import sys
+from typing import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+from bin.compute_frequency import main as main_compute_frequency
+from bin.generate_mapping import main as main_generate_mapping
+from bin.map_edgelist import main as main_map_edgelist
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Map the entities and the relations of an edgelist"
+    )
+    parser.add_argument("edgelist", help="Path of the edgelist file")
+    parser.add_argument("output", help="Path of the output directory")
+    return parser.parse_args()
+
+
+def normalize_args(args: argparse.Namespace) -> None:
+    args.edgelist = os.path.realpath(args.edgelist)
+    args.output = os.path.realpath(args.output)
+
+
+def validate_args(args: argparse.Namespace) -> None:
+    if not os.path.isfile(args.edgelist):
+        print("The edgelist file does not exists")
+        sys.exit(1)
+
+
+def main(args: argparse.Namespace) -> None:
+    args_compute_frequency = argparse.Namespace(
+        edgelist=args.edgelist,
+        ent_freq=os.path.join(args.output, "entities_frequency.tsv"),
+        rel_freq=os.path.join(args.output, "realations_frequency.tsv"),
+        no_header=False,
+    )
+    main_compute_frequency(args_compute_frequency)
+
+    args_generate_mapping = argparse.Namespace(
+        ent_freq=os.path.join(args.output, "entities_frequency.tsv"),
+        rel_freq=os.path.join(args.output, "realations_frequency.tsv"),
+        ent_map=os.path.join(args.output, "entities_mapping.tsv"),
+        rel_map=os.path.join(args.output, "realations_mapping.tsv"),
+    )
+    main_generate_mapping(args_generate_mapping)
+
+    args_map_edgelist = argparse.Namespace(
+        edgelist=args.edgelist,
+        ent_map=os.path.join(args.output, "entities_mapping.tsv"),
+        rel_map=os.path.join(args.output, "realations_mapping.tsv"),
+        mapped_edgelist=os.path.join(args.output, "mapped_edgelist.tsv"),
+    )
+    main_map_edgelist(args_map_edgelist)
+
+
+if __name__ == "__main__":
+    try:
+        ARGS = parse_args()
+
+        normalize_args(ARGS)
+        validate_args(ARGS)
+        main(ARGS)
+    except (KeyboardInterrupt, SystemExit):
+        print("\nAborted!")
